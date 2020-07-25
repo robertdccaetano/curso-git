f802425695a8d433adca72017c9965d53f36139c
Robert Caetano <robertvisualmidia@gmail.com>
2020-07-25 13:35:14 -0300

    Desejando um bom dia ao Senhor Jesus

--- Secao8IntroGitEGitHub/Program.cs
+++ Secao8IntroGitEGitHub/Program.cs
@@ -7,9 +7,8 @@ namespace Secao8IntroGitEGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
-
+            Console.WriteLine("Tenha um bom dia, Senhor Jesus!");
         }
     }
 }
