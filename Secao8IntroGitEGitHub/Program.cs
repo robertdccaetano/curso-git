75c866b6ea4307873bf882d40dff14cd88549cdf
Robert Caetano <robertvisualmidia@gmail.com>
2020-07-25 13:10:39 -0300

    Acrescentada a mensagem pergunta como voce esta

--- Secao8IntroGitEGitHub/Program.cs
+++ Secao8IntroGitEGitHub/Program.cs
@@ -7,6 +7,8 @@ namespace Secao8IntroGitEGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
 
         }
     }
